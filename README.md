e2beb4a4de3c09aaeddcc02ed58381b1e7a091f0
martyna882 <martyna.drabinska@gmail.com>
2019-04-24 21:18:34 +0200

    changed readme

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-lotto simulator with SpringMVC and Hibernate
+#Simulator of lotto drawings
+ 
+ Simple app which allows to conduct a simulation of lotto drawing, 
+ change settings of this simulation and display history 
+ of previous simulations.
+ 
+##Technologies
+ * Java 11
+ * Spring MVC
+ * Hibernate 5
+ * postgreSQL
+ * Thymeleaf
+ * JUnit 4
+ * Tomcat 9
+ * Maven
--- lotto/src/main/java/pl/martyna/lotto/config/HibernateConf.java
+++ lotto/src/main/java/pl/martyna/lotto/config/HibernateConf.java
@@ -45,7 +45,7 @@ public class HibernateConf {
 
     private Properties hibernateProperties() {
         Properties hibernateProperties = new Properties();
-        hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
+        hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "update");
         hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
 
         return hibernateProperties;
